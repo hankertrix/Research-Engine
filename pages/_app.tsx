5e10bd4dc35d91589cff1c160ed6d5664c533f07
hankertrix <91734413+hankertrix@users.noreply.github.com>
2022-07-23 11:57:38 +0000

    Added event listeners for the user's system theme and also loads the user's theme when they load the page so the theme will persist after refresh.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -16,6 +16,35 @@ function MyApp({ Component, pageProps }: AppProps) {
   // The theme state for the application
   const [theme, setTheme] = useState("light");
 
+  // The function to check the user's system theme and the local storage for the theme and set the theme to their preference
+  useEffect(() => {
+
+    // Gets the media query for the user's system theme
+    const themeMq = window.matchMedia("(prefers-color-scheme: dark)");
+
+    // Gets the media query listener
+    const mqListener = e => setTheme(e.matches ? "dark" : "light");
+
+    // Attach an event listener to the user's system theme
+    themeMq.addEventListener("change", mqListener);
+    
+    // Gets the theme in the local storage
+    let themeState: string | null = window.localStorage.getItem("theme");
+
+    // If the theme can't be found
+    if (themeState == null) {
+
+      // Gets the theme from the user's system preferences
+      themeState = themeMq.matches ? "dark" : "light";
+    }
+
+    // Sets the theme to the theme obtained from their system or the local storage
+    setTheme(themeState);
+
+    // Returns the function to remove the event listener
+    return () => themeMq.removeEventListener("change", mqListener);
+  }, [])
+
   // The function to save the state to the local storage when the theme changes
   useEffect(() => window.localStorage.setItem("theme", theme), [theme]);
 
