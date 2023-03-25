13f4e377078f839b01967d22627ff9f27fc8782a
hankertrix <91734413+hankertrix@users.noreply.github.com>
2023-03-25 11:34:54 +0000

    Made GitHub the preferred way to view the source code.
    - Changed the link for the source code in the footer to point to GitHub instead of Replit
    - Rearranged the icons in the footer so that GitHub is on the left and Replit is on the right
    - The reference to the license file in the README is now a GitHub link instead of a Replit one
    - Ignored the cache folder in the .gitignore file

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -19,13 +19,13 @@ const Footer: NextPage<{style?: CSSProperties}> = ({ style }) => {
   
   return (
       <footer className={`${styles.footer} ${themeClass(styles, "footer")} ${styles.flex}`} style={style}>
-        <a href="https://replit.com/@hankertrix/Research-Engine?v=1#LICENSE.txt" title="Copyright notice and license" className={`${styles.copyleft} ${styles.flex}`} rel="noopener noreferrer">
+        <a href="https://github.com/hankertrix/Research-Engine/blob/main/LICENSE.txt" title="Copyright notice and license" className={`${styles.copyleft} ${styles.flex}`} rel="noopener noreferrer">
           <div>Copyleft&nbsp;<span className={styles.symbol}>©</span>&nbsp;2022 Hankertrix.&nbsp;</div> <div>All Wrongs Reserved.</div></a>
         <div className={styles["source-wrapper"]}>
-          <a href="https://replit.com/@hankertrix/Research-Engine?v=1" className={`${styles.source} ${styles.flex}`} title="View source code on Replit" rel="noopener noreferrer">Source:</a>
+          <a href="https://github.com/hankertrix/Research-Engine" className={`${styles.source} ${styles.flex}`} title="View source code on GitHub" rel="noopener noreferrer">Source:</a>
           <div className={`${styles["icon-wrapper"]} ${styles.flex}`}>
+            <a href="https://github.com/hankertrix/Research-Engine" title="View source code on Github" rel="noopener noreferrer"><Image src={githubIcon} width={iconSize} height={iconSize} priority={true} alt="View source code on GitHub" /></a>
             <a href="https://replit.com/@hankertrix/Research-Engine?v=1" title="View source code on Replit" rel="noopener noreferrer"><Image src={replitIcon} width={iconSize} height={iconSize} priority={true} alt="View source code on Replit" /></a>
-            <a href="https://github.com/hankertrix/Research-Engine" title="View source code on Github" rel="noopener noreferrer"><Image src={githubIcon} width={iconSize} height={iconSize} priority={true} alt="View source code on Github" /></a>
           </div>
         </div>
         <div className={styles.theme}>{theme === "light" ? "Light Mode" : "Dark Mode"}</div>
