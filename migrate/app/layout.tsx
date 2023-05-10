36449a1e79e5d785064005db06ae3dc18291de88
hankertrix <91734413+hankertrix@users.noreply.github.com>
2023-05-10 10:22:26 +0000

    Reverted the changes to next.config.js.

--- migrate/app/layout.tsx
+++ migrate/app/layout.tsx
@@ -11,7 +11,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en" className={blackChancery.className}>
+    <html lang="en">
       <body>
         <ThemeContextProvider>{children}</ThemeContextProvider>
       </body>
