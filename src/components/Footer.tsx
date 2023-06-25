5ee1e5832a967be3f5e18434f11dd89c73c9cf75
hankertrix <91734413+hankertrix@users.noreply.github.com>
2023-06-25 15:08:53 +0000

    Added Codeberg as another code source
    Also made Codeberg the preferred method of viewing the source code because Codeberg is just so awesome!

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,8 +7,9 @@ import type { NextPage } from "next";
 import Image from "next/image";
 import { CSSProperties, useContext } from "react";
 import { ThemeContext, ThemeContextType } from "./ThemeContextProvider";
-import replitIcon from "../../public/replit.png";
+import codebergIcon from "../../public/codeberg.png";
 import githubIcon from "../../public/github.png";
+import replitIcon from "../../public/replit.png";
 import styles from "../styles/Footer.module.css";
 
 // The footer
@@ -25,8 +26,9 @@ const Footer: NextPage<{style?: CSSProperties}> = ({ style }) => {
         <a href="https://github.com/hankertrix/Research-Engine/blob/main/LICENSE.txt" target="_blank" title="Copyright notice and license" className={`${styles.copyleft} ${styles.flex}`} rel="noopener noreferrer">
           <div>Copyleft&nbsp;<span className={styles.symbol}>©</span>&nbsp;2023 Hankertrix.&nbsp;</div><div className={styles["rights-notice"]}>All Wrongs Reserved.</div></a>
         <div className={styles["source-wrapper"]}>
-          <a href="https://github.com/hankertrix/Research-Engine" target="_blank" className={`${styles.source} ${styles.flex}`} title="View the source code on GitHub" rel="noopener noreferrer">Source:</a>
+          <a href="https://codeberg.org/Hanker/Research-Engine" target="_blank" className={`${styles.source} ${styles.flex}`} title="View the source code on Codeberg" rel="noopener noreferrer">Source:</a>
           <div className={`${styles["icon-wrapper"]} ${styles.flex}`}>
+            <a href="https://codeberg.org/Hanker/Research-Engine" target="_blank" title="View the source code on Codeberg" rel="noopener noreferrer"><Image src={codebergIcon} width={iconSize} height={iconSize} priority={true} alt="View the source code on Codeberg" /></a>
             <a href="https://github.com/hankertrix/Research-Engine" target="_blank" title="View the source code on GitHub" rel="noopener noreferrer"><Image src={githubIcon} width={iconSize} height={iconSize} priority={true} alt="View the source code on GitHub" /></a>
             <a href="https://replit.com/@hankertrix/Research-Engine?v=1" target="_blank" title="View the source code on Replit" rel="noopener noreferrer"><Image src={replitIcon} width={iconSize} height={iconSize} priority={true} alt="View the source code on Replit" /></a>
           </div>
